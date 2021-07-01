bf01e09e5b54e0bd04bcef9a5dc1033299583df1
rvdrover <53379496+rvdrover@users.noreply.github.com>
2021-07-01 18:02:32 +0530

    name

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -29,10 +29,10 @@ router.get("/", async (req, res) => {
 
 router.delete("/delete", async (req, res) => {
 
-    const filter = {
-        id: req.body.id,
-    };
-    const dataitem = await Todo.deleteOne(filter).then((data) =>
+    // const filter = {
+    //     id: req.body.id,
+    // };
+    const dataitem = await Todo.findByIdAndDelete(req.body._id).then((data) =>
         res.json({
             data: data,
         })

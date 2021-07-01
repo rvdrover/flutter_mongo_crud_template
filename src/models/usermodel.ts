938e3d5fff3cbd3445bfe9715a0af0fba1560675
rvdrover <53379496+rvdrover@users.noreply.github.com>
2021-07-01 16:58:07 +0530

    up

--- src/models/usermodel.ts
+++ src/models/usermodel.ts
@@ -2,15 +2,17 @@ import mongoose from "mongoose";
 
 
 interface TodoDocument extends mongoose.Document {
+    id: string;
     name: string;
     email: string;
     age: number;
 }
 
 const todoSchema = new mongoose.Schema({
-    name: {type: String,required: true},
-    email: {type: String,required: true},
-    age: {type: Number,required: false}
+    id: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    age: { type: Number, required: false }
 });
 
 const Todo = mongoose.model<TodoDocument>(

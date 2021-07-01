eb5ff1c5fb8ee54f02998dc097b8bad0cfa2e0a5
rvdrover <53379496+rvdrover@users.noreply.github.com>
2021-07-01 18:09:46 +0530

    name

--- src/models/usermodel.ts
+++ src/models/usermodel.ts
@@ -9,7 +9,7 @@ interface TodoDocument extends mongoose.Document {
 }
 
 const todoSchema = new mongoose.Schema({
-    _id: { type: String, required: false },
+    _id: mongoose.Types.ObjectId,
     name: { type: String, required: true },
     email: { type: String, required: true },
     age: { type: Number, required: false }
